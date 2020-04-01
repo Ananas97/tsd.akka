1468ae114fda2ca24134b37d154b9489410ff232
baton96 <podolce0@gmail.com>
2020-04-01 18:12:48 +0200

    Lowering death rate

--- Actors/PersonActor.cs
+++ Actors/PersonActor.cs
@@ -60,7 +60,7 @@ namespace TSD.Akka.Actors
 
         private void OnStartDayMessage(StartDayMessage message)
         {
-            if (new Random().NextDouble() < 0.75) Become(Dead);
+            if (new Random().NextDouble() < 0.05) Become(Dead);
             int contacts = random.Next(0, SocialContacts);
             for (int i = 0; i < contacts; i++)
             {
