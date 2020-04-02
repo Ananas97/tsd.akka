315ae8e10c4d6b1d4e5f4aa4b39fd352559b3b4c
piotrhel <piotr.helminiak@snpgroup.com>
2020-04-02 12:24:18 +0200

    implemented paper war

--- Commands/NextDayCommand.cs
+++ Commands/NextDayCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
 using NClap.Metadata;
@@ -9,15 +10,18 @@ namespace TSD.Akka.Commands
     class NextDayCommand : Command
     {
         public ActorSystem System { get; }
+        private static readonly Random Random = new Random();
 
         public NextDayCommand(ActorSystem system) => System = system;
 
         public override Task<CommandResult> ExecuteAsync(CancellationToken cancel)
         {
+            //20% chance for delivery of paper in Biedronka everyday
+            bool paperSupplied = Random.Next(0, 5) == 0;
             var people = System.ActorSelection($"user/{ActorNames.People}/*");
-            people.Tell(new PersonActor.StartDayMessage("Brand new day"));
+            people.Tell(new PersonActor.StartDayMessage("Brand new day", paperSupplied));
             var doctors = System.ActorSelection($"user/{ActorNames.Doctor}/*");
-            doctors.Tell(new PersonActor.StartDayMessage("Brand new day"));
+            doctors.Tell(new PersonActor.StartDayMessage("Brand new day", paperSupplied));
 
             return Task.FromResult(CommandResult.Success);
         }
