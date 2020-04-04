068cbd468a63e9893f79e3c7e638683231b2325b
Jan Techner <jantechner@live.com>
2020-04-04 10:59:34 +0200

    Add doctors treatment

--- Actors/DoctorActor.cs
+++ Actors/DoctorActor.cs
@@ -4,51 +4,32 @@ using Akka.Event;
 
 namespace TSD.Akka.Actors
 {
+    sealed class HealMessage
+    {
+        public string MessageText { get; }
+        public HealMessage(string messageText) => MessageText = messageText;
+    }
+
     class DoctorActor : ReceiveActor
     {
-        public class HealMessage
-        {
-            public string MessageText { get; }
-            public HealMessage(string messageText) => MessageText = messageText;
-        }
 
         private readonly ILoggingAdapter log = Context.GetLogger();
 
         private PersonActor.PersonState state = PersonActor.PersonState.Uninfected;
+        private int numberOfPatientsToday = 0;
         private Random random = new Random();
 
         public DoctorActor()
         {
             // log.Info($"Created actor {Context.Self.Path}");
             Receive<PersonActor.StartDayMessage>(OnStartDayMessage);
-            Receive<PersonActor.InfectedMessage>(OnInfectedMessage);
+            Receive<RequestTreatmentMessage>(OnTreatmentRequest);
         }
 
 
         private void OnStartDayMessage(PersonActor.StartDayMessage message)
         {
-            for (int i = 0; i < 20; i++)
-            {
-                Treatment();
-            }
-        }
-
-        private void Treatment()
-        {
-            var randomPerson = Context.ActorSelection($"/user/{ActorNames.People}");
-            bool personCanBeTreated = random.Next(100) < 15;
-
-            if (state == PersonActor.PersonState.Uninfected)
-            {
-                if (personCanBeTreated)
-                {
-                    randomPerson.Tell(new HealMessage("Lucky you! You received treatment."));
-                }
-            }
-            else if (state == PersonActor.PersonState.Infected)
-            {
-                randomPerson.Tell(new PersonActor.InfectedMessage("Hello, my friend! I'm infected, and I'll infect you too!"));
-            }
+            numberOfPatientsToday = 0;
         }
 
         private void OnInfectedMessage(PersonActor.InfectedMessage message)
@@ -59,10 +40,32 @@ namespace TSD.Akka.Actors
             Become(Infected);
         }
 
+        private void OnTreatmentRequest(RequestTreatmentMessage message)
+        {
+            System.Console.WriteLine(message.MessageText + " " + Sender.Path);
+
+            numberOfPatientsToday++;
+
+            bool personCanBeTreated = random.NextDouble() <= 1 && numberOfPatientsToday <= 5;
+
+            if (state == PersonActor.PersonState.Uninfected && personCanBeTreated)
+            {
+                if (random.NextDouble() <= 0.01) 
+                {
+                    Become(Infected);
+                    System.Console.WriteLine("Doctor is infected");
+                }
+
+                Sender.Tell(new HealMessage("Lucky you! You received treatment."));
+            }
+
+
+
+        }
+
         private void Infected()
         {
             Receive<PersonActor.StartDayMessage>(OnStartDayMessage);
-            Receive<PersonActor.ChatMessage>(message => Sender.Tell(new PersonActor.InfectedMessage("I'm resending you an infection!"), Context.Self));
         }
     }
 }
